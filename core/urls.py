a8e59be14cc767a56d0851569cbbf8ae0d0f5b02
ctomrp <cristian.rico.palma@gmail.com>
2023-06-06 00:23:46 -0400

    avance en templates

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
     path('contact/', contact, name='contact'),
+    path('create_news/', create_news, name='create_news'),
+    path('news_detail/', news_detail, name='news_detail'),
+    path('news_state/', news_state, name='news_state'),
+    path('news_feedback/', news_feedback, name='news_feedback'),
+    path('journalist/', journalist, name='journalist'),
+    path('recover_password/', recover_password, name='recover_password'),
 ]
